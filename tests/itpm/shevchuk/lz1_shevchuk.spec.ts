d7476f227a15f157ea383b0f8609e3f6971ed072
Oleksandr <92521024+OSiZen@users.noreply.github.com>
2024-11-20 10:51:10 +0200

    feature - added new scenario. Shevchuk

---
+++ tests/itpm/shevchuk/lz1_shevchuk.spec.ts
@@ -0,0 +1,12 @@
+import { test } from '@testExtenter';
+import { fakerDataGenerator } from '@utils/helpers/generatedData';
+
+test(`Create new user with valid credentials`, async ({ homePage, registrationPage, headerComponent }) => {
+    const registrationData = fakerDataGenerator.generateNewUserData();
+    registrationData.firstname = 'Oleksandr';
+    registrationData.lastname = 'Shevchuk';
+    await homePage.load();
+    await homePage.clickOnCreateAnAccountLink();
+    await registrationPage.fillAndSubmitRegistrationForm(registrationData);
+    await headerComponent.verifyLoggedInMessage(registrationData.firstname + ' ' + registrationData.lastname);
+});
